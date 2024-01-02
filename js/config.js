628dfecd8828930d71c6f9bb2835be5578761df5
Diana Ivanova <diana.website.developer@gmail.com>
2024-01-02 19:47:36 +0000

    third commit

---
+++ js/config.js
@@ -0,0 +1,5 @@
+
+export const apiKey = 'f990b6370ad73721dfde441e1c3c5700';
+
+export const apiRequestAddress = 'https://api.openweathermap.org/data/2.5/weather';
+
--- js/main.js
+++ js/main.js
@@ -1,136 +1,115 @@
-window.addEventListener('DOMContentLoaded', function () {
-  const preloader = document.querySelector('.card__preloader');
-  const cardWidget = document.querySelector('.card__widget');
 
-  preloader.style.display = 'none';
-  cardWidget.style.display = 'block';
-});
+import { apiKey } from './config.js';
+import { apiRequestAddress } from './config.js';
 
-// modal window
+const preloader = document.querySelector('.card__preloader');
+const cardWidget = document.querySelector('.card__widget');
 
-const modalWindow = document.getElementById('modal');
-const allowLocationBtn = document.getElementById('allowed');
-const denyLocationBtn = document.getElementById('denied');
-const cardInfo = document.getElementById('card-info');
-const accessDenied = document.getElementById('access-denied');
+const button = document.getElementById('button');
+const input = document.getElementById('input');
+const noPermission = document.getElementById('access-denied');
 
-window.onload = openModalWindow;
+// asking permission
 
-function openModalWindow() {
-  modalWindow.style.display = 'block';
+navigator.permissions.query({ name: 'geolocation' })
+  .then(permissionStatus => {
+    console.log(permissionStatus.state);
+
+    if (permissionStatus.state === 'granted' || permissionStatus.state === 'prompt') {
+      localStorage.setItem('geoPermission', 'granted');
+      // Запрос погоды по геолокации
+      getWeatherByGeolocation();
+
+      console.log(localStorage.getItem('geoPermission'));
+    } else if (permissionStatus.state === 'denied') {
+      cardWidget.style.display = 'none';
+      preloader.style.display = 'none';
+      noPermission.style.display = 'block';
+    }
+  })
+  .catch(error => console.error('Permission query error:', error));
+
+// update HTML document
+
+function updateWeatherContent(data) {
+  const temperature = data.main.temp;
+  const weatherDescription = data.weather[0].description;
+  const iconCode = data.weather[0].icon;
+  const weatherIconUrl = `http://openweathermap.org/img/w/${iconCode}.png`;
+  const humidity = data.main.humidity;
+  const windSpeed = data.wind.speed;
+
+  const locationElement = document.getElementById('location');
+  locationElement.innerText = data.name;
+
+  const temperatureElement = document.getElementById('temperature');
+  temperatureElement.innerText = `${Math.round(temperature) - 273}°C`;
+
+  const weatherDescriptionElement = document.getElementById('weather-description');
+  weatherDescriptionElement.innerText = weatherDescription;
+
+  const humidityElement = document.getElementById('humidity');
+  humidityElement.innerText = `${humidity}%`;
+
+  const windElement = document.getElementById('wind-speed');
+  windElement.innerText = `${windSpeed} km/hr`;
+
+  const weatherIconElement = document.getElementById('weather-icon');
+  weatherIconElement.setAttribute('src', weatherIconUrl);
 }
 
-if access denied
+function errorDetection(error) {
+  console.error(error);
+}
 
-denyLocationBtn.onclick = function () {
-  modalWindow.style.display = 'none';
-  cardInfo.style.display = 'none';
-  accessDenied.style.display = 'block';
-};
+// get info from api functions
 
-if access allowed, take user geo position, close modal window
+function getWeatherInfoByGeolocation(position) {
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
 
-const apiKey = 'f990b6370ad73721dfde441e1c3c5700';
+  fetch(`${apiRequestAddress}?lat=${lat}&lon=${lon}&appid=${apiKey}`)
+    .then(response => response.json())
+    .then(data => updateWeatherContent(data))
+    .then(loaderHider)
+    .catch(error => console.error(error));
+}
 
-allowLocationBtn.onclick = function app() {
+function getWeatherByLocation(locationRequest) {
+  fetch(`${apiRequestAddress}?q=${locationRequest}&appid=${apiKey}`)
+    .then(response => response.json())
+    .then(data => updateWeatherContent(data))
+    .catch(error => console.error(error));
+}
 
-  modalWindow.style.display = 'none';
+function getWeatherByGeolocation() {
+  navigator.geolocation.getCurrentPosition(getWeatherInfoByGeolocation, errorDetection);
+}
 
-  function showWeather(position) {
-    let lat = position.coords.latitude;
-    let lon = position.coords.longitude;
+// hide loading div
 
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`)
-      .then(response => response.json())
-      .then(data => {
-        const temperature = data.main.temp;
-        const weatherDescription = data.weather[0].description;
-        const iconCode = data.weather[0].icon;
-        const weatherIconUrl = `http://openweathermap.org/img/w/${iconCode}.png`;
-        const humidity = data.main.humidity;
-        const windSpeed = data.wind.speed;
-        const location = data.name;
+function loaderHider() {
+  preloader.style.display = 'none';
+  cardWidget.style.display = 'block';
+}
 
-        // for HTML
+// event listeners
 
-        const locationElement = document.getElementById('location');
-        locationElement.innerText = location;
-
-        const temperatureElement = document.getElementById('temperature');
-        temperatureElement.innerText = `${Math.round(temperature) - 273}°C`;
-
-        const weatherDescriptionElement = document.getElementById('weather-description');
-        weatherDescriptionElement.innerText = weatherDescription;
-
-        const humidityElement = document.getElementById('humidity');
-        humidityElement.innerText = `${humidity}%`;
-
-        const windElement = document.getElementById('wind-speed');
-        windElement.innerText = `${windSpeed} km/hr`;
-
-        const weatherIconElement = document.getElementById('weather-icon');
-        weatherIconElement.setAttribute('src', weatherIconUrl);
-      })
-      .catch(error => console.error(error));
-  }
-  navigator.geolocation.getCurrentPosition(showWeather, error);
-};
-
-// location entered manually, press enter on keyboard
-
-const input = document.getElementById('input');
-
-input.addEventListener('keyup', function (event) {
+button.addEventListener('click', (event) => {
   event.preventDefault();
-  if (event.keyCode === 13) {
-    button.click();
+  const locationRequest = input.value;
+  if (locationRequest) {
+    getWeatherByLocation(locationRequest);
     input.value = '';
   }
 });
 
-// main function
+input.addEventListener('keyup', (event) => {
+  event.preventDefault();
+  if (event.key === 'Enter') {
+    button.click();
+  }
+});
 
-const button = document.getElementById('button');
 
-button.onclick = function () {
-  const locationRequest = document.getElementById('input').value;
 
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${locationRequest}&appid=${apiKey}`)
-    .then(response => response.json())
-    .then(data => {
-      const temperature = data.main.temp;
-      const weatherDescription = data.weather[0].description;
-      const iconCode = data.weather[0].icon;
-      const weatherIconUrl = `http://openweathermap.org/img/w/${iconCode}.png`;
-      const humidity = data.main.humidity;
-      const windSpeed = data.wind.speed;
-      const location = locationRequest;
-
-      // for HTML
-
-      const locationElement = document.getElementById('location');
-      locationElement.innerText = location;
-
-      const temperatureElement = document.getElementById('temperature');
-      temperatureElement.innerText = `${Math.round(temperature) - 273}°C`;
-
-      const weatherDescriptionElement = document.getElementById('weather-description');
-      weatherDescriptionElement.innerText = weatherDescription;
-
-      const humidityElement = document.getElementById('humidity');
-      humidityElement.innerText = `${humidity}%`;
-
-      const windElement = document.getElementById('wind-speed');
-      windElement.innerText = `${windSpeed} km/hr`;
-
-      const weatherIconElement = document.getElementById('weather-icon');
-      weatherIconElement.setAttribute('src', weatherIconUrl);
-    })
-    .catch(error => console.error(error));
-
-  input.value = '';
-};
-
-function error(error) {
-  console.log(error);
-}
